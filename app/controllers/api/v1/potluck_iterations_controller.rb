8e705e0a634a101fb57178c9e4d401c5ace6ef07
Ryan Knepp <ryan@mirus.io>
2024-09-20 23:49:27 -0500

    updated create for potluck_iterations controller to also handle creating potlucks if passed in

--- app/controllers/api/v1/potluck_iterations_controller.rb
+++ app/controllers/api/v1/potluck_iterations_controller.rb
@@ -15,9 +15,33 @@ class Api::V1::PotluckIterationsController < ApplicationController
 
   # POST /potluck_iterations
   def create
-    @potluck_iteration = PotluckIteration.new(date_range: dates_to_range(params[:date_from], params[:date_to]))
+    date_range = dates_to_range(params[:date_from], params[:date_to])
+    @potluck_iteration = PotluckIteration.find_or_initialize_by(date_range: date_range, organization: current_org)
 
     if @potluck_iteration.save
+      # create potlucks if passed in
+      if params.has_key?(:potlucks)
+        potlucks = params[:potlucks]
+        potlucks.each do |p|
+          if !p[:host_household_id].nil? || !p[:host_person_id].nil?
+            potluck = @potluck_iteration.potlucks.new(organization: current_org, host_household_id: p[:host_household_id], host_person_id: p[:host_person_id])
+            households = []
+            people = []
+            p[:attendees].each do |a|
+              if a[:household_id]
+                households.append(a[:household_id])
+              end
+              if a[:person_id]
+                people.append(a[:person_id])
+              end
+            end
+            potluck.household_ids=households
+            potluck.person_ids=people
+            potluck.save
+          end
+        end
+      end
+
       render json: @potluck_iteration, status: :created
     else
       render json: @potluck_iteration.errors, status: :unprocessable_entity
