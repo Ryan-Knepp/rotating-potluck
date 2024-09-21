91efed8fcb6e54f8907445d9245f4bea5e173717
Ryan Knepp <ryan@mirus.io>
2024-09-20 23:45:40 -0500

    make sure people are included when rendering json for household

--- app/models/household.rb
+++ app/models/household.rb
@@ -2,4 +2,8 @@ class Household < ApplicationRecord
   belongs_to :organization
   has_many :people
   has_and_belongs_to_many :potlucks
+
+  def as_json(options = { include: :people })
+    super(options)
+  end
 end
