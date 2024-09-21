d581fd78c69552c62a16be1fe5a38de58917e2ff
Ryan Knepp <ryan@mirus.io>
2024-09-20 23:44:47 -0500

    routes updates for new controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
+      resources :me
       resources :potluck_iterations
       resources :potlucks
       resources :organizations
@@ -9,11 +10,18 @@ Rails.application.routes.draw do
       resources :people do
         get "search", on: :collection
       end
+      resources :attendees
     end
   end
 
-  get "/login" => "auth#login"
-  get "/logout" => "auth#logout"
+  # pseudo login using personal access token
+  get "login" => "auth#login"
+  get "logout" => "auth#logout"
+
+  # actual oauth2 login
+  get "auth/login" => "oauth#login"
+  get "auth/complete" => "oauth#complete"
+  get "auth/logout" => "oauth#logout"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
